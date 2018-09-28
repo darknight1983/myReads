d67307c31498fb9880d8fa7402343b458de1cc36
Jacolby Green <darknightnov@gmail.com>
2018-09-28 11:32:25 -0500

    search updated w/ Lodash debounce

--- client/src/searchComponent.js
+++ client/src/searchComponent.js
@@ -1,6 +1,8 @@
 import React,{ Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Debounce } from 'react-throttle';
+import { debounce } from 'lodash';
 
 import Book from './bookComponent';
 
@@ -13,17 +15,20 @@ export default class SearchComp extends Component {
     }
 
     this.handleSearch = this.handleSearch.bind(this)
-    this.handleRequest = this.handleRequest.bind(this)
+    this.handleRequest = debounce(this.handleRequest.bind(this), 300)
+
   }
   handleSearch(e) {
     this.setState({
       searchTerm: e.target.value
     })
+    console.log(this.state.searchTerm)
   }
   handleRequest(e) {
-    if (e.charCode === 13) {
-      this.props.findBook(this.state.searchTerm)
-    }
+    // if (e.charCode === 13) {
+    //
+    // }
+    this.props.findBook(this.state.searchTerm)
   }
   render() {
     const { books, updateCategory } = this.props;
@@ -40,12 +45,16 @@ export default class SearchComp extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input
-               type="text"
-               placeholder="Search by title or author"
-               value={this.state.searchTerm}
-               onChange={this.handleSearch}
-               onKeyPress={this.handleRequest}/>
+
+              <input
+                 type="text"
+                 placeholder="Search by title or author"
+                 value={this.state.searchTerm}
+                 onChange={this.handleSearch}
+                 onKeyPress={this.handleRequest}
+                 />
+
+
 
           </div>
         </div>
